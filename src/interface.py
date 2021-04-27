7c6d39ab29ae90581910681442a6d34b0634c75b
Afeez Agbaje <afeezagbaje@gmail.com>
2021-04-27 19:34:18 +0100

    Added an interface for the project

---
+++ src/interface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class IReadingFiles(ABC):
+    @abstractmethod
+    def checking_valid_format(self):
+        pass
+
+    @abstractmethod
+    def iterator_content(self):
+        pass
+
+    @abstractmethod
+    def read_content(self):
+        pass
+
+    @abstractmethod
+    def first_two_rows(self):
+        pass
+
+    @abstractmethod
+    def last_two_rows(self):
+        pass
