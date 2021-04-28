6a5002821f73ed1aac24c51eeaef441ec9f0986f
Afeez Agbaje <afeezagbaje@gmail.com>
2021-04-28 12:16:51 +0100

    Added some functions to my main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from src.ReadBytesFromFile import ReadBytesFromFile
 process = ProcessSpreadSheet('')
 read = ReadBytesFromFile('')
 
-
 if __name__ == '__main__':
     process.read_content()
     read.read_content()
