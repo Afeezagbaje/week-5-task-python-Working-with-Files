1712f4acab22af89b1eb62d2c693ce0620800525
Afeez Agbaje <afeezagbaje@gmail.com>
2021-04-28 12:15:58 +0100

    requirement.txt was add to the project

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src.ProcessSpreadsheet import ProcessSpreadSheet
+from src.ReadBytesFromFile import ReadBytesFromFile
 
 
 process = ProcessSpreadSheet('')
+read = ReadBytesFromFile('')
+
+
+if __name__ == '__main__':
+    process.read_content()
+    read.read_content()
